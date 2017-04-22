80c7c0d4e9b834822452090a96e88f6a598e2bca
reynoldcn <308388682@qq.com>
2017-04-23 00:45:00 +0800

    add comments

--- src/main.c
+++ src/main.c
@@ -19,7 +19,9 @@ pthread_t g_astPthread[MAX_LIST];
 char g_done[MAX_LIST+1];    //I should've used atomic_t instead of char*, but unfortunately
                             //atomic_t is only defined in Linux Kernel.
 
-
+/*
+初始化全局变量，没啥说的。
+*/
 void ListInit(void)
 {
     UINT i = 0;
@@ -53,7 +55,11 @@ void MapThreadInit(void)
         pthread_create(&g_astPthread[i], NULL, (void *)TaskMapProc, (void *)i);
     }
 }
-
+/*
+主线程，负责把raw data从log文件里读出，放入队列里
+每一个队列都有一个线程负责处理，处理后的东西统一写入一个
+hash表，最后把hash表里的东西拿出来，排好序，就是最终产品
+*/
 void MainThread_ReadFile(char *filename)
 {
     FILE *fp = NULL;
@@ -83,6 +89,9 @@ void MainThread_ReadFile(char *filename)
         
             //PRINT_LOGITEM(pstLogItemNew, "get");
     }
+    //当整个log读完之后，没有额外分配一个消息结构体用于通知各线程
+    //只是发送一个fResponseTime为负的元素进去（正常不会出现）
+    //只要线程读到这个，就知道文件已经读完了，可以跳出了
     pstLogItem->fResponseTime = -1.0f;
     for (int i = 0; i < MAX_LIST; ++i)
     {
@@ -93,6 +102,9 @@ void MainThread_ReadFile(char *filename)
     }
     //sleep(5);
     g_done[MAX_LIST] = '\0';
+    //每个队列读完后，都会把g_done字符串中的第n位置1，
+    //当都是1，说明所有的队列都读完了，结束线程。
+    //后续如果修改了队列数目MAX_LIST,这里也要改
     while (0 != strcmp(g_done, "11111111")) {
         usleep(1);
     }
@@ -117,16 +129,18 @@ int main(int argc, const char * argv[])
     else if (argc > 2)
     {
         printf("Too many arguments\n");
+        return -1;
     }
-    //printf("%d, %s, %s\n", argc, argv[0], filename);
+    else if (argc < 2)
+    {
+        printf("No Parameters Set, Use Default\n");
+    }
 #endif
 
     ListInit();
     ResultInit();
     MapThreadInit();
-
     MainThread_ReadFile(filename);
-    sleep(5);
 #ifdef SHOW_RESULT
     int i = 0;
     for (; i < HASH_LEN; ++i) {
